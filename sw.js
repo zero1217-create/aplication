8a44c81ba796955d8369499d4a95246df951e6f5
zero1217-create <u971965b@ecs.osaka-u.ac.jp>
2025-07-09 18:06:52 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,35 @@
+const VERSION = 'v1';
+const CACHE_NAME = `quiz-pwa-${VERSION}`;
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => res || fetch(e.request))
+  );
+});
 
